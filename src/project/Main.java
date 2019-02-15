035b79db00d845725cfdd480293249e1b74b622b
ptam1 <puitm3@gmail.com>
2019-02-14 21:15:16 -0500

    first commit

---
+++ src/project/Main.java
@@ -0,0 +1,29 @@
+package project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+
+public class Main extends Application
+{
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+
+        Pane pane = FXMLLoader.load(getClass().getResource("project.fxml"));
+        stage.setTitle("Employee Scheduler");
+        stage.setScene(new Scene(pane));
+        stage.show();
+    }
+
+    
+
+}
